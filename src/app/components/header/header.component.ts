f33e188437a3eb331b401cb1ea5be810752cdb33
Jamshed Latipov <jamshedlatipov@gmail.com>
2024-10-24 21:13:10 +0500

    fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, DestroyRef, } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ArticleResponse, Country } from '../../models/article';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { ArticleService } from '../../services/article.service';
 import { DropdownModule } from 'primeng/dropdown';
@@ -10,7 +10,15 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { uniqBy } from 'lodash';
 import { environment } from '../../../environments/environment';
 import { RouterModule } from '@angular/router';
-
+const DEFAULT_COUNTRY = {
+  id: 1000000,
+  attributes: {
+    "name": 'Tajikistan',
+    "code": 'tajikistan',
+    delivery: "850",
+    cities: [],
+  }
+}
 
 @Component({
   selector: 'app-header',
@@ -20,24 +28,19 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  public countries$: Observable<Country> | undefined;
+  public countries:Country | undefined;
   public results$: Observable<any> | undefined;
   public backURL = environment.backUrl;
-  selectedCountry: any = {
-    attributes: {
-      "name": 'Tajikistan',
-      "code": 'tajikistan',
-    }
-  };
   searchControl: FormControl = new FormControl();
+  countryControl: FormControl = new FormControl(DEFAULT_COUNTRY);
+  DEFAULT_COUNTRY = DEFAULT_COUNTRY;
 
   constructor(private articleService: ArticleService, private destroyRef: DestroyRef) {
-    this.countries$ = this.articleService.getCountries().pipe(
-      map((data) => {
-        data.data.unshift(this.selectedCountry);
-        data.data = uniqBy(data.data, (i) => i.attributes.code);
-        return data;
-      })
+
+    this.articleService.getCountries().subscribe((data) => {
+        this.countries = data;
+        this.countryControl.setValue(this.countries.data[0]);
+      }
     );
 
     this.results$ = this.searchControl.valueChanges.pipe(
