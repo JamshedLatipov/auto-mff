3b5580c67de62abad8405c06b03da4a7e8bfa4e3
jamshed <jamshedlatipov@gmail.com>
2024-10-30 18:26:20 +0500

    f

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path: 'detail/:id', loadComponent: () => import('./pages/detail/detail.component').then((m) => m.DetailComponent), pathMatch: 'full' },
     { path: 'detail', redirectTo: '/' },
-    { path: 'detail/:id', loadComponent: () => import('./pages/detail/detail.component').then((m) => m.DetailComponent) },
     { path: '', loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent) },
 ];
